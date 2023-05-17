f1693d3ee26a1f0242dd95ee886f83d00aebde43
ummnn <juniorsam@naver.com>
2023-05-17 14:50:38 +0900

    f

---
+++ app/src/main/java/com/example/survive_all/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.survive_all;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
